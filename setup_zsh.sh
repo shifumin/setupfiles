5931803f4949938254fa0a31bd365f1c2a61cf16
shifumin <shifumin4230@gmail.com>
2016-08-13 23:17:42 +0900

    add install oh-my-zsh

--- setup_zsh.sh
+++ setup_zsh.sh
@@ -14,3 +14,6 @@ cd /usr/local/src/zsh-${ZSH_VER}
 
 sudo sh -c "echo '/usr/local/bin/zsh' >> /etc/shells"
 chsh -s /usr/local/bin/zsh
+
+# install oh-my-zsh
+[ ! -d ~/.oh-my-zsh ] && git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
