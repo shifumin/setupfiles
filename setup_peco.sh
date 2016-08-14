8dd5da99cdcf7992d8e0f7fc4a9f0f1199d65284
shifumin <shifumin4230@gmail.com>
2016-08-14 18:06:03 +0900

    add settings for anyframe

--- setup_peco.sh
+++ setup_peco.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# peco
 PECO_VER="0.3.6"
 
 cd /usr/local/src
@@ -9,3 +10,8 @@ cd /usr/local/src
 [ ! -e ~/.peco/config.json ] && mkdir $HOME/.peco && ln -s $HOME/dotfiles/config.json $HOME/.peco
 
 sudo ln -s /usr/local/src/peco_linux_amd64/peco /usr/local/bin
+
+# anyframe
+mkdir $HOME/.zsh/
+cd $HOME/.zsh/
+git clone git@github.com:mollifier/anyframe.git
