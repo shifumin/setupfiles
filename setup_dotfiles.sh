801d1c85a4ac41325da63a13913c0d21433c3756
shifumin <shifumin4230@gmail.com>
2016-12-27 22:28:09 +0900

    add common-settings.vim

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -4,20 +4,23 @@
 DOT_FILES=( ansible.cfg .ctags .gemrc .gitconfig .gitignore .gitmessage.txt .pryrc .rubocop.yml .tigrc .tmux.conf .vimrc .vimrc.neobundle .zshrc .zshrc.custom )
 for file in ${DOT_FILES[@]}
 do
-    ln -s $HOME/dotfiles/$file $HOME/$file
+    ln -s $HOME/dotfiles/$file $HOME/
 done
 
 # install bin for tmux
 BIN_FILES=( battery )
 for file in ${BIN_FILES[@]}
 do
-    ln -s $HOME/dotfiles/$file /usr/local/bin/$file
+    ln -s $HOME/dotfiles/$file /usr/local/bin/
 done
 
-# install .vim/indent/**.vim
-[ ! -d ~/.vim/indent ] && mkdir ~/.vim/indent
-ln -s $HOME/dotfiles/.vim/indent/ruby.vim $HOME/.vim/indent/ruby.vim
-ln -s $HOME/dotfiles/.vim/indent/php.vim $HOME/.vim/indent/php.vim
+# install $HOME/.vim/indent/*.vim
+[ ! -d $HOME/.vim/indent ] && mkdir -p $HOME/.vim/indent
+ln -s $HOME/dotfiles/.vim/indent/ruby.vim $HOME/.vim/indent/
+ln -s $HOME/dotfiles/.vim/indent/php.vim $HOME/.vim/indent/
 
-# install .vim/syntax/smarty.vim
-[ ! -d ~/.vim/syntax ] && mkdir ~/.vim/syntax && ln -s $HOME/dotfiles/.vim/syntax/smarty.vim $HOME/.vim/syntax/smarty.vim
+# install $HOME/.vim/syntax/smarty.vim
+[ ! -d $HOME/.vim/syntax ] && mkdir -p $HOME/.vim/syntax && ln -s $HOME/dotfiles/.vim/syntax/smarty.vim $HOME/.vim/syntax/
+
+# install $HOME/.vim/after/plugin/common-settings.vim
+[ ! -d $HOME/.vim/after/plugin ] && mkdir -p $HOME/.vim/after/plugin && ln -s $HOME/dotfiles/.vim/after/plugin/common-settings.vim $HOME/.vim/after/plugin/
