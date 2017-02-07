3c7cfef03846a0368996ad926d54f000c6347fc8
shifumin <shifumin4230@gmail.com>
2017-02-08 02:21:01 +0900

    comment out settings for installing peco

--- setup_peco.sh
+++ setup_peco.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-# peco
-PECO_VER="0.4.2"
-
-cd /usr/local/src
-
-[ ! -e /usr/local/src/peco_linux_amd64.tar.gz ] && sudo wget https://github.com/peco/peco/releases/download/v${PECO_VER}/peco_linux_amd64.tar.gz
-[ ! -e /usr/local/src/peco_linux_amd64 ] && sudo tar zxvf peco_linux_amd64.tar.gz
-[ ! -e ~/.peco/config.json ] && mkdir $HOME/.peco && ln -s $HOME/dotfiles/config.json $HOME/.peco
-
-sudo ln -s /usr/local/src/peco_linux_amd64/peco /usr/local/bin
-
-# anyframe
-mkdir $HOME/.zsh/
-cd $HOME/.zsh/
-git clone git@github.com:mollifier/anyframe.git
+# # peco
+# PECO_VER="0.4.7"
+#
+# cd /usr/local/src
+#
+# [ ! -e /usr/local/src/peco_linux_amd64.tar.gz ] && sudo wget https://github.com/peco/peco/releases/download/v${PECO_VER}/peco_linux_amd64.tar.gz
+# [ ! -e /usr/local/src/peco_linux_amd64 ] && sudo tar zxvf peco_linux_amd64.tar.gz
+# [ ! -e ~/.peco/config.json ] && mkdir $HOME/.peco && ln -s $HOME/dotfiles/config.json $HOME/.peco
+#
+# sudo ln -s /usr/local/src/peco_linux_amd64/peco /usr/local/bin
+#
+# # anyframe
+# mkdir $HOME/.zsh/
+# cd $HOME/.zsh/
+# git clone git@github.com:mollifier/anyframe.git
