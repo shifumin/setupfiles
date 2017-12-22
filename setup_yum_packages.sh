5b771d62fa61df32f5495d4ce936c5477f3f614c
shifumin <shifumin4230@gmail.com>
2017-12-22 23:53:44 +0900

    update setup_yum_packages.sh

--- setup_yum_packages.sh
+++ setup_yum_packages.sh
@@ -2,12 +2,7 @@
 
 sudo yum -y install epel-release
 
-sudo yum -y install gcc wget kernel ntp
-sudo yum -y install logwatch
+sudo yum -y install gcc curl wget
+sudo yum -y install kernel ntp logwatch
 sudo yum -y install mod24_ssl openssl openssl-devel
-
 sudo yum -y install httpd
-sudo yum -y install postgresql-server
-
-# setup_git.sh
-# sudo yum -y install git golang
